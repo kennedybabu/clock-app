d40cb3be126064e62c7d43d1345b3a01a08ab03f
kennedybabu <kennedynjogu8@gmail.com>
2022-07-03 10:20:05 +0300

    feature: change the time format, remove the secs

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,8 +9,9 @@ const Hero = () => {
     useEffect(() => {
       axios.get('http://worldtimeapi.org/api/ip').then((response)=> {
           setTime(response.data)
-          console.log(time)
+         
           setGreetingText()
+          getDayTime()
           console.log('time', greeting)
       }).catch((error) => {
         console.log(error.message)
@@ -22,9 +23,30 @@ const Hero = () => {
 
     const [greeting, setGreeting] = useState('')
 
-    let d = saa.toLocaleTimeString()
+    let d = saa.toLocaleTimeString('en-US', {
+      hour: '2-digit',
+      minute: '2-digit',
+    })
+
+
+    function padTo2Digits(num) {
+      return String(num).padStart(2, '0')
+    }
+
+    const hoursAndMinutes = padTo2Digits(saa.getHours()) + ':' + padTo2Digits(saa.getMinutes())
 
     let hrs = saa.getHours()
+
+    let mins = saa.getMinutes()
+
+    const [dayTime, SetDayTime ] = useState(true)
+
+
+    const getDayTime = () => {
+      if(hrs >= 18 && hrs <= 23) {
+        SetDayTime(!dayTime)
+      }
+    }
    
     const setGreetingText = () => {
       if(hrs < 12) {
@@ -34,26 +56,49 @@ const Hero = () => {
       } else {
         setGreeting('Good Evening')
       }
-    }
+    }    
     
   return (
     <div>
+        {dayTime ? 
         <div className='w-full h-screen day relative'>
-            <div className="overlay absolute w-full h-full left-0 top-0"></div>
-            {/* container */}
-            <div className='w-full text-white h-full absolute left-0 top-0 flex flex-col justify-end pb-[100px] px-[30px]'>
-                <div className='w-full flex items-center text-white'><BsBrightnessHighFill  className='mr-2'/> {greeting}</div>
-                <h2 className='text-5xl font-bold'>{d} <span className='text-white text-xl'>{time.abbreviation}</span></h2>
-                <p className='uppercase font-bold'>In {time.timezone}</p>
+        <div className="overlay absolute w-full h-full left-0 top-0"></div>
+        {/* container */}
+        <div className='w-full text-white h-full absolute left-0 top-0 flex flex-col justify-end'>
+            <div className='px-4 w-full '>
+                <div className='text-xl tracking-wider uppercase w-full flex items-center text-white'><BsBrightnessHighFill  className='mr-2'/> {greeting}</div>
+                  <h2 className='text-[90px] font-bold'>{hoursAndMinutes} <span className='text-white text-xl'>{time.abbreviation}</span></h2>
+                <p className='uppercase font-bold py-4'>In {time.timezone}</p>
                 <div className='items-center flex w-[90px] py-[5px] justify-between px-2 bg-white uppercase text-gray-800 rounded-[25px]'>
                   <div>more</div> 
                   <div><IoIosArrowDropdownCircle className='text-xl'/></div>
                 </div>
-                <div>
+            </div>
+            <div className='hidden backdrop-blur-md p-2 text-black w-full mt-2 h-[45vh] bg-[#d6cfcf65]'>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur voluptatibus cupiditate ratione nam! Eius, aspernatur.</p>
+            </div>
+        </div>            
+    </div> 
+    : 
+    <div className='w-full h-screen night relative'>
+            <div className="overlay absolute w-full h-full left-0 top-0"></div>
+            {/* container */}
+            <div className='w-full text-white h-full absolute left-0 top-0 flex flex-col justify-end'>
+                <div className="px-4 w-full">
+                <div className='text-xl tracking-wider uppercase w-full flex items-center text-white'><BsBrightnessHighFill  className='mr-2'/> {greeting}</div>
+                <h2 className='text-[90px] font-bold'>{hrs}:{mins} <span className='text-white text-xl'>{time.abbreviation}</span></h2>
+                <p className='uppercase font-bold py-4'>In {time.timezone}</p>
+                <div className='items-center flex w-[90px] py-[5px] justify-between px-2 bg-white uppercase text-gray-800 rounded-[25px]'>
+                  <div>more</div> 
+                  <div><IoIosArrowDropdownCircle className='text-xl'/></div>
+                </div>
+                </div>
+                <div className='backdrop-blur-lg p-2 h-[45vh]  bg-[#00000065] mt-2'>
                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur voluptatibus cupiditate ratione nam! Eius, aspernatur.</p>
                 </div>
             </div>            
         </div>
+        }
     </div>
   )
 }
